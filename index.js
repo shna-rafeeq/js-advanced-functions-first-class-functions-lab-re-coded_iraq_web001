5f2a5031ff9077e0596250f89b0c9f3670b2bd0f
shna rafeeq <shnar.majeed@gmail.com>
2020-06-09 08:57:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(arr = []){
+ let newarr = [...arr];
+ newarr.splice(2);
+ return(newarr);
+}
+returnFirstTwoDrivers(drivers);
+
+const returnLastTwoDrivers = function(arr = []){
+return(arr.slice(2));
+}
+returnLastTwoDrivers(drivers);
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+  return function(y = 1){
+    if(y == 1){
+       return x * x;
+    }
+   else{
+     return x * y;
+   }
+  }
+}
+  const fareMultiplier = createFareMultiplier(4);
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  function selectDifferentDrivers( arr = [], x = func){
+    return x(arr);
+  }
+ 
+  selectDifferentDrivers(drivers,returnLastTwoDrivers)
